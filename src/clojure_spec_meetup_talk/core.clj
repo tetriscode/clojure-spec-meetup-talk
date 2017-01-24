8fea050a8094efa5591a24827e5ed44cab81e1a7
Wes Richardet <wes@tetriscodes.com>
2017-01-24 17:08:02 -0600

    Adding regex email

--- src/clojure_spec_meetup_talk/core.clj
+++ src/clojure_spec_meetup_talk/core.clj
@@ -1,14 +1,13 @@
 (ns clojure-spec-meetup-talk.core
-  (:require [clojure.spec :as spec]
-            [clojure.spec.gen :as gen]
+  (:require [clojure.spec :as s]
             [clojure.string :as str]
             [com.gfredericks.test.chuck.generators :as genc]
-            ))
+            [clojure.test.check.generators :as gen]))
 
 ;; First up, validation with predicates
-;; (spec/valid? <predicates> 10)
-(spec/valid? even? 10)
-(spec/valid? #(> (count %) 0) "2017-01-01T01:01:01Z")
+;; (s/valid? <predicates> 10)
+(s/valid? even? 10)
+(s/valid? #(> (count %) 0) "2017-01-01T01:01:01Z")
 
 ; Defining a spec. Specs for GeoJSON http://geojson.org
 ;{
@@ -21,62 +20,67 @@
 ;               "name": "Dinagat Islands"
 ;               }
 ;}
-(spec/def ::longitude double?)
-(spec/def ::latitude double?)
-(spec/def :wgs/coordinates (spec/tuple ::longitude ::latitude))
-(spec/valid? :wgs/coordinates [1.0 2.0])
+(s/def ::longitude double?)
+(s/def ::latitude double?)
+(s/def :wgs/coordinates (s/tuple ::longitude ::latitude))
+(s/valid? :wgs/coordinates [1.0 2.0])
 
-(spec/explain :wgs/coordinates [1.8 2.8])
+(s/explain :wgs/coordinates [1.8 2.8])
 (def test-val [:test 2.8])
-(spec/explain :wgs/coordinates test-val)
-(spec/explain-str :wgs/coordinates test-val)
-(spec/explain-data :wgs/coordinates test-val)
-(spec/describe :wgs/coordinates)
-(spec/* :wgs/coordinates)
+(s/explain :wgs/coordinates test-val)
+(s/explain-str :wgs/coordinates test-val)
+(s/explain-data :wgs/coordinates test-val)
+(s/describe :wgs/coordinates)
+(s/def ::seq (s/* int?))
+(s/valid? ::seq [:key "foo" 1])
+(s/conform ::seq [:key "foo" 1])
+(s/valid? ::seq [1 3 4])
+(s/conform ::seq [0])
+(s/valid? ::seq [])
 
-(spec/def :cart/coordinates (spec/cat :x ::longitude :y ::latitude))
+(s/def :cart/coordinates (s/cat :x ::longitude :y ::latitude))
 
-(gen/generate (spec/gen :wgs/coordinates)) ; vec
-(type (clojure.spec.gen/generate (spec/gen :wgs/coordinates))) ; vec
-(type (clojure.spec.gen/generate (spec/gen :cart/coordinates))) ; seq
+(gen/generate (s/gen :wgs/coordinates)) ; vec
+(type (clojure.spec.gen/generate (s/gen :wgs/coordinates))) ; vec
+(type (clojure.spec.gen/generate (s/gen :cart/coordinates))) ; seq
 
-(spec/def ::type #{"Point"})
+(s/def ::type #{"Point"})
 
-(spec/def ::geometry (spec/keys :req-un [::type :wgs/coordinates]))
+(s/def ::geometry (s/keys :req-un [::type :wgs/coordinates]))
 
-(gen/generate (spec/gen ::geometry))
+(gen/generate (s/gen ::geometry))
 
-(gen/sample (spec/gen ::geometry))
+(gen/sample (s/gen ::geometry))
 
-(spec/def ::longitude (spec/double-in :min -180.0 :max 180 :NaN false :infinite? false))
-(spec/def ::latitude (spec/double-in :min -90.0 :max 90 :NaN false :infinite? false))
-(spec/def ::coordinates (spec/tuple ::longitude ::latitude))
-(spec/def ::geometry (spec/keys :req-un [::type ::coordinates]))
+(s/def ::longitude (s/double-in :min -180.0 :max 180 :NaN false :infinite? false))
+(s/def ::latitude (s/double-in :min -90.0 :max 90 :NaN false :infinite? false))
+(s/def ::coordinates (s/tuple ::longitude ::latitude))
+(s/def ::geometry (s/keys :req-un [::type ::coordinates]))
 
-(gen/sample (spec/gen ::geometry))
+(gen/sample (s/gen ::geometry))
 
-(spec/def :feature/type #{"Feature"})
-(spec/def ::properties map?)
+(s/def :feature/type #{"Feature"})
+(s/def ::properties map?)
 
-(spec/def ::feature (spec/keys :req-un [:feature/type ::properties]))
-(gen/sample (spec/gen ::feature))
+(s/def ::feature (s/keys :req-un [:feature/type ::properties]))
+(gen/sample (s/gen ::feature))
 
-(spec/def ::properties (spec/map-of string? (spec/or :s string? :n number? :m map? :c coll?)))
+(s/def ::properties (s/map-of string? (s/or :s string? :n number? :m map? :c coll?)))
 
-(spec/def ::jsonobj (spec/map-of string? (spec/or :s string? :n number? :m ::jsonobj :c (spec/coll-of ::jsonobj))))
+(s/def ::jsonobj (s/map-of string? (s/or :s string? :n number? :m ::jsonobj :c (s/coll-of ::jsonobj))))
 
-;(spec/def ::properties ::jsonobj) ; pegging the CPU time
-(spec/def ::id (spec/or :p pos-int? :s (spec/and string? #(not (str/blank? %)))))
-(spec/def ::feature (spec/keys :req-un [::id :feature/type ::properties]))
+;(s/def ::properties ::jsonobj) ; pegging the CPU time
+(s/def ::id (s/or :p pos-int? :s (s/and string? #(not (str/blank? %)))))
+(s/def ::feature (s/keys :req-un [::id :feature/type ::properties]))
 
-(gen/sample (spec/gen ::feature))
+(gen/sample (s/gen ::feature))
 
 ; namespace point
-(spec/def :pt/type #{"Point"})
-(spec/def :pt/geometry (spec/keys :req-un [:pt/type ::coordinates]))
-(spec/def :pt/feature (spec/keys :req-un [::id :pt/type :pt/geometry ::properties]))
+(s/def :pt/type #{"Point"})
+(s/def :pt/geometry (s/keys :req-un [:pt/type ::coordinates]))
+(s/def :pt/feature (s/keys :req-un [::id :pt/type :pt/geometry ::properties]))
 ; namespace poly
-(spec/def :poly/type #{"Polygon"})
+(s/def :poly/type #{"Polygon"})
 
 (defn circle-gen [x y]
   (let [vertices (+ (rand-int 8) 4)
@@ -87,38 +91,59 @@
                     (+ y (* radius (Math/sin (* rads r))))])
                  (range vertices))]
     (conj pts (last pts))))
-(spec/def :poly/coordinates (spec/with-gen
+(s/def :poly/coordinates (s/with-gen
                            coll?
                            #(gen/fmap (fn [[lon lat]] (list (circle-gen lon lat)))
-                                      (gen/tuple (spec/gen ::longitude) (spec/gen ::latitude)))))
-(spec/def :poly/geometry (spec/keys :req [:poly/type :poly/coordinates]))
-(spec/def :poly/feature (spec/keys :req-un [::id :poly/geometry :poly/type ::properties]))
+                                      (gen/tuple (s/gen ::longitude) (s/gen ::latitude)))))
+(s/def :poly/geometry (s/keys :req [:poly/type :poly/coordinates]))
+(s/def :poly/feature (s/keys :req-un [::id :poly/geometry :poly/type ::properties]))
 
-(spec/def :feat/geometry (spec/or :poly/feature :pt/feature))
-(spec/def ::feature (spec/keys :req-un [::id :feature/type :feat/geometry ::properties]))
+(s/def :feat/geometry (s/or :poly/feature :pt/feature))
+(s/def ::feature (s/keys :req-un [::id :feature/type :feat/geometry ::properties]))
 
-(gen/sample (spec/gen :poly/feature))
+(gen/sample (s/gen :poly/feature))
 
-(spec/def ::feature-spec (spec/keys :req-un
+(s/def ::feature-spec (s/keys :req-un
                                  [:gfeature/id :gfeature/type
                                   :gj/geometry :gfeature/properties]))
 
-(spec/def :gj/features (spec/coll-of ::feature-spec))
-(spec/def :fc/type #{"FeatureCollection"})
-(spec/def ::featurecollection-spec (spec/keys :req-un [:fc/type :gj/features]))
+(s/def :gj/features (s/coll-of ::feature-spec))
+(s/def :fc/type #{"FeatureCollection"})
+(s/def ::featurecollection-spec (s/keys :req-un [:fc/type :gj/features]))
 
-(spec/def ::string-test (spec/with-gen #(> (count %) 200)
-                                      gen/string-alphanumeric))
+(s/def ::string-test (s/with-gen #(> (count %) 200)
+                                 gen/string-alphanumeric))
 
-(gen/sample (spec/gen ::string-test)) ; custom gen?
+(gen/sample (s/gen ::string-test)) ; custom gen?
 
-(spec/def ::string-test (spec/with-gen #(> (count %) 0)
-                                       gen/string-alphanumeric))
-(gen/sample (spec/gen ::string-test)) ; custom gen?
+(s/def ::string-test (s/with-gen #(> (count %) 0)
+                                 gen/string-alphanumeric))
+(gen/sample (s/gen ::string-test)) ; custom gen?
 
+
+(def email-regex #"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$")
+(s/def :user/email-type (s/and string? #(re-matches email-regex %)))
+(s/def :user/email :user/email-type)
+(s/def :user/password string?)
+(s/def :user/name string?)
+(s/def ::user-spec (s/keys :req-un [:user/email :user/password]
+                           :opt-un [:user/name]))
+
+(gen/sample (s/gen ::user-spec) )
 
 (def sqlcol-regex #"[a-z][a-z0-9_]*")
-(spec/def ::regex (spec/with-gen
-                          (spec/and string? #(> (count %) 0) #(re-matches sqlcol-regex %))
-                          #(genc/string-from-regex sqlcol-regex)))
-(gen/sample (spec/gen ::regex)) ; custom gen?
+(s/def ::regex (s/with-gen
+                 (s/and string? #(> (count %) 0) #(re-matches sqlcol-regex %))
+                 #(genc/string-from-regex sqlcol-regex)))
+(gen/sample (s/gen ::regex)) ; custom gen?
+
+(def email-regex #"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}")
+(s/def :user/email-type (s/with-gen
+                          (s/and string? #(re-matches email-regex %))
+                          #(genc/string-from-regex email-regex))) ; no anchors
+(s/def ::user-spec (s/keys :req-un [:user/email :user/password]
+                           :opt-un [:user/name]))
+
+(gen/sample (s/gen ::user-spec))
+
+
